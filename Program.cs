e34f81863481d5cb1dd74c9c72aa1d3ce2bac293
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2022-10-25 23:34:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // intializing
+            Plane testPlane = new Plane();
+            Plane plane1 = new Plane("MO26", PlaneType.Jet, 8, 10);
+
+            plane1.AddASeat();
+            plane1.AddASeat();
+            plane1.ShowInfo();
+
+            Plane plane2 = new Plane("TB700", PlaneType.Turboprop, 120, 200);
+            plane2.RemoveASeat();
+            plane2.ShowInfo();
+        }
+    }
+}
